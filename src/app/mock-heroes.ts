e376cb6c34b220f6af9dfa5845f67dc8bf4358ec
KouseiImai <kouseiimai16340@gmail.com>
2021-04-13 07:11:51 +0900

    Create heroes-mock

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES:Hero[] = [
+  { id: 11, name:'Ishiro'},
+  { id: 12, name:'Jirou'},
+  { id: 13, name:'Saburou'},
+  { id: 14, name:'Gorou'},
+  { id: 15, name:'Tarou'},
+  { id: 16, name:'Ken'},
+  { id: 17, name:'Yuuji'},
+  { id: 18, name:'Masaru'},
+  { id: 19, name:'Yuuki'},
+  { id: 20, name:'Tetuto'},
+];
